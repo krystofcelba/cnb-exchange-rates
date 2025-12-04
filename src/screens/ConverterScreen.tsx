59375c4a986e5801cdc77b1e780f170af41f8832
Krystof Celba <krystof.celba@icloud.com>
2025-12-04 18:22:30 +0100

    feat: implement exchange rates list on homescreen

--- src/screens/ConverterScreen.tsx
+++ src/screens/ConverterScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { RootStackParamList } from '../navigation/AppNavigator';
+
 export const ConverterScreen = () => {
+    const route = useRoute<RouteProp<RootStackParamList, 'Converter'>>();
+    const { currency } = route.params;
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Converter Screen</Text>
+            <Text style={styles.text}>Converter: {currency.code}</Text>
         </View>
     );
 };
